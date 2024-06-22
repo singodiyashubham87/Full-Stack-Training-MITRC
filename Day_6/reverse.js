f6d0c4260db87d5b236e1c514e34933d18bda448
singodiyashubham87 <singodiyashubham87@gmail.com>
2024-06-22 14:36:07 +0530

    chore: add day 11

---
+++ Day_6/reverse.js
@@ -0,0 +1,33 @@
+// function reverse(str)
+// {
+//     const x = str.split(" ").reverse().join(" ")
+//     console.log(x)
+// }
+
+// function largestword(str) {
+//     var word = str.split(" ");
+//     var max = word[0];
+//     for (var i = 1; i < word.lenght; i++) {
+//         if (word[i].lenght > max.lenght) {
+//             max = word[i];
+//         }
+//     }
+//     console.log(max)
+//     console.log(word)
+
+// }
+// largestword(" Hello my name is Hussain")
+
+// const points = [40, 30, 12, 65, 53, 1, 5, 25, 10]
+// points.sort(function (a, b) { return a - b })
+// console.log(points)
+
+// const arr = ['bmw','porsche', 'audi','honda', 'lexus', 'pagani', 'astonmartin']
+// x=arr.sort()
+// x=arr.push('bmw')
+// console.log(x)
+
+//   squar
+const number = [1,2,3,4,5]
+let x= number.map((item)=>item*item)
+console.log(x)
